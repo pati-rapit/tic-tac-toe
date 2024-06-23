eeb4e352c5e9aa49bd5905b2352d045d6a4d07ee
pati-rapit <oredash8tundere@gmail.com>
2024-06-23 18:21:28 +0900

    fix github pagesの設定中

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const production = process.env.NODE_ENV === 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -20,7 +22,10 @@ const config = {
 			fallback: 'index.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: production ? '/tic-tac-toe' : '',
+		},
 	}
 };
 
